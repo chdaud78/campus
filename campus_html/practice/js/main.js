0eb9f11bbb164d9b45e62d92dc11b8f9ceb626cd
박총명 <qkrri56@naver.com>
2023-06-04 16:59:21 +0900

    [ENH] Chapter 1-9
    Chpater 1-9 Finish

--- campus_html/practice/js/main.js
+++ campus_html/practice/js/main.js
@@ -12,3 +12,31 @@ console.log(email);
 console.log(hello);
 console.log(user.name);
 
+let boxEl = document.querySelector('.box');
+
+console.log(boxEl);
+
+boxEl.addEventListener('click', function () {
+  console.log('Click');
+  boxEl.classList.add('active');
+  console.log(
+      boxEl.classList.contains('active')
+  );
+  boxEl.classList.remove('active');
+  console.log(
+      boxEl.classList.contains('active')
+  );
+});
+
+const boxEls = document.querySelectorAll('.boxes');
+
+boxEls.forEach(function (boxEl, index) {
+  boxEl.classList.add(`order-${index + 1}`);
+  console.log(index,boxEl);
+});
+
+console.log(boxEl.textContent);
+
+boxEl.textContent = 'PCM';
+
+console.log(boxEl.textContent)
