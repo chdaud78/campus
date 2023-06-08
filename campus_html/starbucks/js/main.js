077aa4ebcfe4ec72707c23d5514cb7c92cb16c05
박총명 <qkrri56@naver.com>
2023-06-08 19:07:38 +0900

    [ENH] Add Header
    add and styling header (logo, sub menu, main menu, badges)
    
    add library (lodash, gsap)
    
    add search function, badges function

--- campus_html/starbucks/js/main.js
+++ campus_html/starbucks/js/main.js
@@ -14,3 +14,23 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log("scroll!");
+  if(window.scrollY > 500) {
+    // 배지 숨기기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none',
+    })
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block',
+    })
+  }
+}, 300));
+// _.throttle(함수, 시간); -> 시간마다 함수 실행 제한
